00dfcb1d42c16157be6be4ab47d04022ebd17ff8
Felipe Duarte <coalajedi.ios@gmail.com>
2023-08-14 16:27:47 -0300

    :sparkles: implements Tiling generative art tool
    Signed-off-by: Felipe Duarte <coalajedi.ios@gmail.com>

--- README.md
+++ README.md
@@ -1,10 +1,54 @@
-# generative_art
+# Generative Art
 
-A new Flutter project.
+Inspired by Vera Molnar’s artistic style and ideas, this project go over some basic principles of generative art by
+exploring Flutter’s CustomPainter and the Canvas API, and introduce animations to make the artwork come to life.
 
-## Getting Started
+## Who?
 
-This project is a starting point for a Flutter application.
+Vera Molnar, a great Hungarian artist based in France who is now in her 90’s and still produces work. She is considered
+one of the pioneers of computer generated generative art.
+
+From a young age, Molnar was fascinated by randomness and was one of the early adapters of using algorithms to render
+artwork, and in a time when computer generated art was claimed to be dehumanized and its authenticity highly doubted,
+she argued that working with the randomness in machines and their ability to efficiently produce a high number of
+outputs was in fact proof of the opposite of those arguments.
+
+Because when you work with a computer, you’re using it as a tool to achieve a closer alignment with your own
+imagination. You are using your creativity and curiosity and expressing yourself in a medium that gives you more space
+for exploration than manual labor could ever provide.
+
+Over the years, Molnar’s methods and equipment for producing generative artwork kept evolving. In fact, as early as a
+decade before she even had access to a computer, she was already creating “generative” art.
+
+By definition, generative art is any practice where the artist uses a system, such as a set of natural language rules, a
+computer program, or a machine, and then sets it into motion with some degree of autonomy.
+
+In the 50s, she introduced us to the term “Machine Imaginere” or the imaginary machine. She used fundamental principles
+from mathematics along with analog ‘randomness generators’ such as a roll of dice. In this way, she ‘produced’ sequences
+of drawings, making modifications to a single parameter at a time. She would then meticulously handcraft each of these
+variations.
+
+After she gained access to computers, she initially worked with IBM mainframes that didn’t even have monitors. As she
+transitioned to more advanced systems, she used programming languages like BASIC and Fortran to feed the machine with
+specific instructions that would control a connected pen plotter. The pen plotter would then use an ink pen and a robot
+arm to translate the code into drawings.
+
+## Generative Art Tools
+
+There are a couple of tools involved when it comes to creating generative artwork, and I want to briefly mention some
+of those tools implemented with the Canvas API.
+
+## Tiling - Generative Art Tool #1
+
+Like tiling a wall, you tile pieces of your artwork by repeating them horizontally and vertically. We do that by
+calculating the number of shapes (square in this case) that can fit horizontally and vertically in the Widget that
+contains the `CustomPainter`, and draw the shapes using a simple `for` loop.
+
+Result:
+
+![Tiling - Generative Art Tool](assets/tiling_generative_art_tool.mov)
+
+## Run the project
 
 A few resources to get you started if this is your first Flutter project:
 
@@ -14,3 +58,9 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+Once you have the Flutter environment settled up. Clone this repo by:
+
+```shell
+git clone https://github.com/coalajedi/generative_art.git
+```
---
+++ lib/squares_grid.dart
@@ -0,0 +1,127 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2023 Felipe D. Silva (a.k.a Coala Jedi)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import 'package:flutter/material.dart';
+
+class SquaresGridWidget extends StatelessWidget {
+  const SquaresGridWidget({
+    super.key,
+    this.sideLength = 80,
+    this.strokeWidth = 3,
+    this.gap = 30,
+  });
+
+  final double sideLength;
+  final double strokeWidth;
+  final double gap;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: ColoredBox(
+        color: Colors.black,
+        child: SizedBox.expand(
+          child: CustomPaint(
+            painter: SquaresCustomPainter(
+              sideLength: sideLength,
+              strokeWidth: strokeWidth,
+              gap: gap,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SquaresCustomPainter extends CustomPainter {
+  SquaresCustomPainter({
+    this.sideLength = 80,
+    this.strokeWidth = 3,
+    this.gap = 30,
+  });
+
+  final double sideLength;
+  final double strokeWidth;
+  final double gap;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Colors.deepPurple
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = strokeWidth;
+
+    /// Calculate the number of squares that can fit on the horizontal axis
+    final xCount = ((size.width + gap) / (sideLength + gap)).floor();
+
+    /// Calculate the number of squares that can fit on the vertical axis
+    final yCount = ((size.height + gap) / (sideLength + gap)).floor();
+
+    /// Calculate the size of the grid of squares
+    final contentSize = Size(
+      (xCount * sideLength) + ((xCount - 1) * gap),
+      (yCount * sideLength) + ((yCount - 1) * gap),
+    );
+
+    /// Calculate the offset from which we should start painting the grid so
+    /// that it is eventually centered
+    final offset = Offset(
+      (size.width - contentSize.width) / 2,
+      (size.height - contentSize.height) / 2,
+    );
+
+    final totalCount = xCount * yCount;
+
+    /// Save the current status of the canvas
+    canvas.save();
+
+    /// Translate the canvas to it's center offset
+    canvas.translate(
+      offset.dx,
+      offset.dy,
+    );
+    for (int index = 0; index < totalCount; index++) {
+      int i = index ~/ yCount;
+      int j = index % yCount;
+
+      canvas.drawRect(
+        Rect.fromLTWH(
+          (i * (sideLength + gap)),
+          (j * (sideLength + gap)),
+          sideLength,
+          sideLength,
+        ),
+        paint,
+      );
+    }
+
+    canvas.restore(); // Reset the canvas
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
